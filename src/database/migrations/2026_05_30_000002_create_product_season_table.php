629baff72629e2d8234329bb52cc4fc9dccdd68a
s_onozawa <sc7gp1q@gmail.com>
2026-06-13 20:39:59 +0900

    修正分コミット

--- src/database/migrations/2026_05_30_000002_create_product_season_table.php
+++ src/database/migrations/2026_05_30_000002_create_product_season_table.php
@@ -15,8 +15,8 @@ class CreateProductSeasonTable extends Migration
     {
         Schema::create('product_season', function (Blueprint $table) {
             $table->id();
-            $table->id('product_id');
-            $table->id('season_id');
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('season_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
